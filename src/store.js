51f09d9f0f82a455aa16a96ab13efe8a788e9a8c
Gabriel Paredes <paredesgabrielb@gmail.com>
2024-10-15 16:57:22 -0400

    create store.js file

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore, Reducer } from "@reduxjs/toolkit";
+import cartReducer from './Components/CartSlice';
+
+const store = configureStore({
+    reducer: {
+        cart: cartReducer
+    }
+})
+
+export default store;
