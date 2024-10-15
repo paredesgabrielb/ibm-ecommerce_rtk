8cc18b3c29209683aa2a9162d5b0d6a22cfd0038
Gabriel Paredes <paredesgabrielb@gmail.com>
2024-10-15 17:25:39 -0400

    shoppingcart functionalities not render

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeItemFromCart,clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice';
 import './ShoppingCart.css'; 
 
-const ShoppingCart = () => {
 
+const ShoppingCart = () => {
+  const dispatch = userDispatch();
+  const  cartItems = useSelector(state => state.cart.cartItems);
+  let totalAmount = cartItems.reduce((total,current) => total + current.price*current.quantity,0);
+
+  const handleRemoveItem= itemId => {
+    dispatch(removeItemFromCart(itemId));
+  }
+
+  const handleCleanCart = () => {
+    dispatch(clearCart());
+  }
+
+  const handleIncreaseItemQuantity = itemId => {
+    dispatch(increaseItemQuantity(itemId));
+  }
+
+  const handleDecreaseItemQuantity = itemId => {
+    dispatch(decreaseItemQuantity(itemId));
+  }
+  
   return (
     <>
     <div className="shopping-cart">
       <h2 className="shopping-cart-title">Shopping Cart</h2>
       <ul className="cart-items">
-       
+       {cartItems.map( item =>(
+        <li key={item.id} className='cart-item'>
+            
+        </li>
+        ))}
       </ul>
       <button className="clear-cart-btn">Clear Cart</button>
     </div>
